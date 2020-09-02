13278e93aca8d579d8dc6152e905eba14bddec17
Fbada006 <ferdinandkamata@yahoo.com>
2020-09-02 21:40:18 +0300

    add accessories fragment

---
+++ app/src/main/java/com/droidafricana/protocodetest/main/AccessoriesFragment.kt
@@ -0,0 +1,22 @@
+package com.droidafricana.protocodetest.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.droidafricana.protocodetest.databinding.FragmentAccessoriesBinding
+
+class AccessoriesFragment : Fragment() {
+
+    private lateinit var binding: FragmentAccessoriesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAccessoriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
